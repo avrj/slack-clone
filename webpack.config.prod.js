8c4ced8eecaa4ec37e5f7b8bbda0292217645c33
avrj <anraiha@cs.helsinki.fi>
2017-02-04 18:55:09 +0200

    added DedupePlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -19,6 +19,7 @@ module.exports = {
             }
         }),
         new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin()
     ],
 
