e9e425b81dd8ccc30d8828552a13964dbcf06075
Anton Räihä <avrj@users.noreply.github.com>
2018-02-24 10:58:26 +0200

    Remove DedupePlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,7 +18,6 @@ module.exports = {
                 NODE_ENV: JSON.stringify('production')
             }
         }),
-        new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin()
     ],
 
