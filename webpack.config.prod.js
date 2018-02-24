f54b3bfc820be46290676496016180d2c27be3d5
Anton Räihä <avrj@users.noreply.github.com>
2018-02-24 10:53:19 +0200

    Remove OccurenceOrderPlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,7 +18,6 @@ module.exports = {
                 NODE_ENV: JSON.stringify('production')
             }
         }),
-        new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin()
     ],
