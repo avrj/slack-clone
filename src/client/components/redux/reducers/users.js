9fc2abd2f9de2fa09744abe73715e8c2e40a0e51
Anton Räihä <anraiha@cs.helsinki.fi>
2018-03-05 20:55:27 +0200

    Handle user online event

--- src/client/components/redux/reducers/users.js
+++ src/client/components/redux/reducers/users.js
@@ -2,6 +2,7 @@ export const types = {
   FETCH_USERS_REQUEST: 'USERS/FETCH_USERS_REQUEST',
   FETCH_USERS_SUCCESS: 'USERS/FETCH_USERS_SUCCESS',
   FETCH_USERS_FAILURE: 'USERS/FETCH_USERS_FAILURE',
+  SET_USER_ONLINE: 'USERS/SET_USER_ONLINE',
 }
 
 const initialState = {}
@@ -18,6 +19,21 @@ export default (state = initialState, action) => {
       }, {})
 
       return { ...state, users }
+    case types.SET_USER_ONLINE:
+      if (state.users[action.user]) {
+        return {
+          ...state,
+          users: {
+            ...state.users,
+            [user]: { ...state.users[user], online: true },
+          },
+        }
+      } else {
+        return {
+          ...state,
+          users: { ...state.users, [user]: { online: true, messages: [] } },
+        }
+      }
     default:
       return state
   }
@@ -27,4 +43,5 @@ export const actions = {
   fetchUsers: () => ({ type: types.FETCH_USERS_REQUEST }),
   setUsers: users => ({ type: types.FETCH_USERS_SUCCESS, users }),
   fetchFailed: error => ({ type: types.FETCH_USERS_FAILURE, error }),
+  setUserOnline: user => ({ type: types.SET_USER_ONLINE, user }),
 }
