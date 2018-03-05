c1e9b4b15c902afc329acade50637d6bf865fa2f
Anton Räihä <anraiha@cs.helsinki.fi>
2018-03-05 20:13:26 +0200

    Add PersistGate & Redux Provider

--- src/client/components/AppContainer.js
+++ src/client/components/AppContainer.js
@@ -2,11 +2,18 @@ import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { node } from 'prop-types'
 import { CookiesProvider } from 'react-cookie'
+import { Provider } from 'react-redux'
+import { store, persistor } from './redux/stores/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const AppContainer = ({ children }) => (
-  <MuiThemeProvider>
-    <CookiesProvider>{children}</CookiesProvider>
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <MuiThemeProvider>
+        <CookiesProvider>{children}</CookiesProvider>
+      </MuiThemeProvider>
+    </PersistGate>
+  </Provider>
 )
 
 AppContainer.propTypes = {
